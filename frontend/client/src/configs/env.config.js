656549e162f38abc9380680440315eb336f733e5
Joy Owusu Ansah <owusujoyansah@gmail.com>
2026-06-24 21:10:56 +0000

    Vite

--- frontend/client/src/configs/env.config.js
+++ frontend/client/src/configs/env.config.js
@@ -1,3 +1,3 @@
 export const ENV = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/v1',
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://car-cedp.onrender.com/api/v1',
 };
